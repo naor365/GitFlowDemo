01e873c3f29ed30bb1e0fde654b6aab596af0e0e
Naor Shlomo <Naor.Shlomo@kornit.com>
2021-06-22 11:57:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitFlowDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
